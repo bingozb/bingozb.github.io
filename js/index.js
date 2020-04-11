1083fefce3536f7796abebcb4a87b2d9def12929
bingo <bingov5@icloud.com>
2020-04-11 22:43:45 +0800

    66

---
+++ js/index.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.documentElement.getElementsByClassName('hero')[0].remove();
+    document.documentElement.getElementsByClassName('footer-wrapper footer')[0].firstElementChild.remove();
+}
